f13d78cfad413b25fe8a5d02c814523ecfddeda2
Neal Robben <neal.robben@gmail.com>
2015-08-23 13:40:33 +0200

    Updated feedback from download

--- RapidGatorDownload/DownloadItem.cs
+++ RapidGatorDownload/DownloadItem.cs
@@ -10,7 +10,18 @@ public class DownloadItem : DependencyObject
     private WebClient client;
 
     public string DownloadName { get; set; }
-    //public int Progress { get; set; }
+
+    public int Progress
+    {
+        get { return (int)GetValue(ProgressProperty); }
+        set { SetValue(ProgressProperty, value); }
+    }
+
+    public string ProgressFeedback
+    {
+        get { return (string)GetValue(ProgressFeedbackProperty); }
+        set { SetValue(ProgressFeedbackProperty, value); }
+    }
 
     public DownloadItem(UserInfo usrInfo, DownloadInfo dlInfo)
     {
@@ -19,6 +30,7 @@ public class DownloadItem : DependencyObject
 
         DownloadName = dlInfo.FileName;
         Progress = 0;
+        ProgressFeedback = "";
     }
 
     public void DownLoad()
@@ -49,23 +61,24 @@ public class DownloadItem : DependencyObject
 
     private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
     {
-
+        if (e.Cancelled)
+        {
+            ProgressFeedback = "Download cancelled";
+        }
+        else
+        {
+            ProgressFeedback = "Download complete";
+        }
     }
 
     private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
     {
         Progress = e.ProgressPercentage;
-        //feedbackLabel.Content = $"{e.ProgressPercentage}% ({e.BytesReceived / 1000000}MB/{e.TotalBytesToReceive / 1000000}MB) ";
-    }
-
-    public int Progress
-    {
-        get { return (int)GetValue(ProgressProperty); }
-        set { SetValue(ProgressProperty, value); }
+        ProgressFeedback = $"Downloading {e.ProgressPercentage}% ({e.BytesReceived / 1000000}MB/{e.TotalBytesToReceive / 1000000}MB))";
     }
 
     // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-    public static readonly DependencyProperty ProgressProperty =
-        DependencyProperty.Register("Progress", typeof(int), typeof(DownloadItem), new PropertyMetadata(0));
+    public static readonly DependencyProperty ProgressProperty = DependencyProperty.Register("Progress", typeof(int), typeof(DownloadItem), new PropertyMetadata(0));
+    public static readonly DependencyProperty ProgressFeedbackProperty = DependencyProperty.Register("ProgressFeedback", typeof(string), typeof(DownloadItem), new PropertyMetadata(""));
 
 }
